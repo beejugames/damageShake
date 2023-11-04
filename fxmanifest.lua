4c896c110437af160e008f83e311496b8bc7f1e4
beeju <44073986+beejugames@users.noreply.github.com>
2023-11-04 16:56:01 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+authors 'beeju - https://discord.gg/Kze4pBreVz'
+description 'shake cam by beeju'
+version '1.0'
+
+-- tebex — https://prentiss.tebex.io/category/scripts
+
+lua54 'yes'
+
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'client/client.lua',
+}
